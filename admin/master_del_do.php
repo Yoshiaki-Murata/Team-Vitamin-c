8bba6536c23f3c907fd071a61257ac3dfb7fc5cc
Sawako Okoba <taakiwami@gmail.com>
2026-04-16 15:29:52 +0900

    管理者削除実装

---
+++ admin/master_del_do.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__ . '/../inc/function.php';
+
+// TODO: データ受け取り
+if (!empty($_GET)) {
+
+    $id = $_GET['id'];
+
+    // DBに接続
+    try {
+        $db = db_connect();
+        // 削除するSQL
+        $sql = 'DELETE FROM admins WHERE id=:id';
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':id', $id);
+
+        $stmt->execute();
+
+        // トップページへ画面遷移
+        header('location:index.php');
+        exit();
+    } catch (PDOException $e) {
+        exit('エラー: ' . $e->getMessage());
+    }
+}
