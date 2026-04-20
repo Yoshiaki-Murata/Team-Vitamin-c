621c7339305978352e0d64b8f47584a76d4a1808
Ehara <mnrehr0913@gmail.com>
2026-04-20 12:35:23 +0900

    何か変えた

--- inc/header_student.php
+++ inc/header_student.php
@@ -2,35 +2,35 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- リセットCSS -->
-    <link rel="stylesheet" href="https://unpkg.com/destyle.css@3.0.2/destyle.min.css">
-    <link rel="stylesheet" href="./../css/style.css">
-    <!-- bootstrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
-    <title>キャリコン予約</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- リセットCSS -->
+  <link rel="stylesheet" href="https://unpkg.com/destyle.css@3.0.2/destyle.min.css">
+  <link rel="stylesheet" href="./../css/style.css">
+  <!-- bootstrap -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+  <title>キャリコン予約</title>
 </head>
 
 <body>
-    <header>
-        <nav class="bg-body-tertiary">
-            <ul class="nav justify-content-center">
-                <li class="nav-item">
-                    <a href="./index.php" class="nav-link text-reset">
-                        トップページ
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="./request.php" class="nav-link text-reset">
-                        プラス新規予約
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="./logout_do.php" class="btn  btn-outline-secondary">
-                        ログアウト
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+  <header class="l-header">
+    <nav class="bg-body-tertiary">
+      <ul class="nav justify-content-center">
+        <li class="nav-item">
+          <a href="./index.php" class="nav-link text-reset">
+            トップページ
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="./request.php" class="nav-link text-reset">
+            プラス新規予約
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="./logout_do.php" class="btn  btn-outline-secondary">
+            ログアウト
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
