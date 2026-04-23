0f31548c165436f74338d089dd5e2d2647b656d8
Miharu Matsuura <miharu03291020@gmail.com>
2026-04-23 13:33:24 +0900

    学生トップページ出し分け修正途中

--- student/index.php
+++ student/index.php
@@ -3,23 +3,33 @@ require_once __DIR__ . "/../inc/function.php";
 
 $name = $_SESSION["user_name"];
 $login_id = $_SESSION["user_id"];
+$course_id = $_SESSION["user_course_id"];
 $db = db_connect();
 try {
-  // 必須キャリコンの情報を取得
-  $sql_must = "SELECT ri.id,ti.time,rsl.date,mt.name AS method_name,cl.name AS class_name FROM reservation_infos ri 
+  if ($course_id === 1) {
+    // 必須キャリコンの情報を取得
+    $sql_must = "SELECT ri.id,ti.time,rsl.date,mt.name AS method_name,cl.name AS class_name FROM reservation_infos ri 
             INNER JOIN reservation_slots rsl ON ri.slot_id = rsl.id
             INNER JOIN methods mt ON ri.method_id= mt.id
             INNER JOIN times ti ON rsl.time_id = ti.id
             LEFT JOIN classes cl ON rsl.class_id = cl.id
             LEFT JOIN carecons cr ON rsl.carecon_id =cr.id
             WHERE ri.student_id=:login_id AND rsl.carecon_id=1";
-  $stmt_must = $db->prepare($sql_must);
-  $stmt_must->bindParam(":login_id", $login_id, PDO::PARAM_INT);
-  $stmt_must->execute();
-  $result_must = $stmt_must->fetchAll(PDO::FETCH_ASSOC);
+    $stmt_must = $db->prepare($sql_must);
+    $stmt_must->bindParam(":login_id", $login_id, PDO::PARAM_INT);
+    $stmt_must->execute();
+    $result_must = $stmt_must->fetchAll(PDO::FETCH_ASSOC);
 
-  // キャリコンプラスの情報を取得
-  $sql_plus = "SELECT ri.id,ti.time,rsl.date,mt.name AS method_name,cl.name AS class_name FROM reservation_infos ri 
+    // 必須キャリコンの開催日の情報を取得する
+    $sql_cr = "SELECT DISTINCT rs.date
+    FROM reservation_slots rs 
+    WHERE rs.carecon_id=1";
+    $stmt_cr = $db->prepare($sql_cr);
+    $stmt_cr->execute();
+    $cr = $stmt_cr->fetchAll(PDO::FETCH_ASSOC);
+  } else {
+    // キャリコンプラスの情報を取得
+    $sql_plus = "SELECT ri.id,ti.time,rsl.date,mt.name AS method_name,cl.name AS class_name FROM reservation_infos ri 
             INNER JOIN reservation_slots rsl ON ri.slot_id = rsl.id
             INNER JOIN methods mt ON ri.method_id= mt.id
             INNER JOIN times ti ON rsl.time_id = ti.id
@@ -27,18 +37,11 @@ try {
             LEFT JOIN carecons cr ON rsl.carecon_id =cr.id
             WHERE ri.student_id=:login_id AND rsl.carecon_id=2";
 
-  $stmt_plus = $db->prepare($sql_plus);
-  $stmt_plus->bindParam(":login_id", $login_id, PDO::PARAM_INT);
-  $stmt_plus->execute();
-  $result_plus = $stmt_plus->fetchAll(PDO::FETCH_ASSOC);
-
-  // 必須キャリコンの開催日の情報を取得する
-  $sql_cr = "SELECT DISTINCT rs.date
-    FROM reservation_slots rs 
-    WHERE rs.carecon_id=1";
-  $stmt_cr = $db->prepare($sql_cr);
-  $stmt_cr->execute();
-  $cr = $stmt_cr->fetchAll(PDO::FETCH_ASSOC);
+    $stmt_plus = $db->prepare($sql_plus);
+    $stmt_plus->bindParam(":login_id", $login_id, PDO::PARAM_INT);
+    $stmt_plus->execute();
+    $result_plus = $stmt_plus->fetchAll(PDO::FETCH_ASSOC);
+  }
 } catch (PDOException $e) {
   echo "エラ‐" . $e->getMessage();
 }
@@ -60,49 +63,50 @@ try {
         <button type="button" class="btn btn-primary mb-3" id="mReserveBtn">スケジュール</button>
       </div>
 
-      <div>
-        <?php if ($result_must): ?>
-          <table class="table ms-4">
-            <thead>
-              <tr class="row">
-                <th class="col-2">日付</th>
-                <th class="col-2">開始時間</th>
-                <th class="col-3">面談方法</th>
-                <th class="col-2">教室</th>
-                <th class="col-3">操作</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php foreach ($result_must as $rm): ?>
+      <?php if ($course_id === 1): ?>
+        <div>
+          <?php if ($result_must): ?>
+            <table class="table ms-4">
+              <thead>
                 <tr class="row">
-                  <td class="col-2">
-                    <?php echo $rm["date"]; ?>
-                  </td>
-                  <td class="col-2">
-                    <?php echo $rm["time"]; ?>
-                  </td>
-                  <td class="col-3">
-                    <?php echo $rm["method_name"]; ?>
-                  </td>
-                  <td class="col-2">
-                    <?php echo $rm["class_name"] ?? "未定"; ?>
-                  </td>
-                  <td class="col-3">
-                    <form action="./change_request.php" method="post">
-                      <input type="hidden" name="reserve-id" id="reserve-id" value="<?php echo $rm["id"] ?>">
-                      <input type="submit" value="変更申請" class="btn btn-sm btn-danger">
-                    </form>
-                  </td>
+                  <th class="col-2">日付</th>
+                  <th class="col-2">開始時間</th>
+                  <th class="col-3">面談方法</th>
+                  <th class="col-2">教室</th>
+                  <th class="col-3">操作</th>
                 </tr>
-              <?php endforeach; ?>
-            </tbody>
-          </table>
-        <?php else: ?>
-          <p>実施の予定はありません</p>
-        <?php endif; ?>
-      </div>
+              </thead>
+              <tbody>
+                <?php foreach ($result_must as $rm): ?>
+                  <tr class="row">
+                    <td class="col-2">
+                      <?php echo $rm["date"]; ?>
+                    </td>
+                    <td class="col-2">
+                      <?php echo $rm["time"]; ?>
+                    </td>
+                    <td class="col-3">
+                      <?php echo $rm["method_name"]; ?>
+                    </td>
+                    <td class="col-2">
+                      <?php echo $rm["class_name"] ?? "未定"; ?>
+                    </td>
+                    <td class="col-3">
+                      <form action="./change_request.php" method="post">
+                        <input type="hidden" name="reserve-id" id="reserve-id" value="<?php echo $rm["id"] ?>">
+                        <input type="submit" value="変更申請" class="btn btn-sm btn-danger">
+                      </form>
+                    </td>
+                  </tr>
+                <?php endforeach; ?>
+              </tbody>
+            </table>
+          <?php else: ?>
+            <p>実施の予定はありません</p>
+          <?php endif; ?>
+        </div>
     </div>
-
+  <?php else: ?>
     <div class="mb-4">
       <div class="d-flex gap-3">
         <h2 class="mb-3 c-title_plus">キャリコンプラス予約状況</h2>
@@ -152,28 +156,29 @@ try {
       </div>
 
     </div>
+  <?php endif; ?>
 
-    <dialog class="dialog">
-      <div class="mb-4">
-        <h2 class="text-center mb-5">
-          キャリコン　予約情報
-        </h2>
-        <select name="modalDate" id="modalDate" class="form-select mx-auto w-50 mb-4">
-          <?php foreach ($cr as $c): ?>
-            <option value="<?php echo $c["date"]; ?>"><?php echo $c["date"]; ?></option>
-          <?php endforeach; ?>
-        </select>
-        <table class="table" id="modalTable">
-          <thead>
-            <th class="text-center">時間帯</th>
-            <th class="text-center">予約者</th>
-          </thead>
-          <tbody>
-          </tbody>
-        </table>
-      </div>
-      <button class="btn btn-secondary mx-auto" id="closeModal">閉じる</button>
-    </dialog>
+  <dialog class="dialog">
+    <div class="mb-4">
+      <h2 class="text-center mb-5">
+        キャリコン　予約情報
+      </h2>
+      <select name="modalDate" id="modalDate" class="form-select mx-auto w-50 mb-4">
+        <?php foreach ($cr as $c): ?>
+          <option value="<?php echo $c["date"]; ?>"><?php echo $c["date"]; ?></option>
+        <?php endforeach; ?>
+      </select>
+      <table class="table" id="modalTable">
+        <thead>
+          <th class="text-center">時間帯</th>
+          <th class="text-center">予約者</th>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    </div>
+    <button class="btn btn-secondary mx-auto" id="closeModal">閉じる</button>
+  </dialog>
 
   </main>
   <script src="./../js/student.js"></script>
