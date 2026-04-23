d6690e9f1cba89fe3c62925fc57d5696d31c3675
Miharu Matsuura <miharu03291020@gmail.com>
2026-04-23 15:27:56 +0900

    プレースホルダ修正

--- admin/login.php
+++ admin/login.php
@@ -35,7 +35,7 @@ require_once __DIR__ . "/../inc/function.php";
             <div class="row justify-content-center">
                 <div class="mb-3 col-6">
                     <label for="login_id" class="form-laber">ログインID</label>
-                    <input type="text" name="login_id" id="login_id" class="form-control" autocomplete="login_id" placeholder="半角英数字10字">
+                    <input type="text" name="login_id" id="login_id" class="form-control" autocomplete="login_id" placeholder="半角英数字10字以内">
                 </div>
             </div>
             <div class="row justify-content-center">
